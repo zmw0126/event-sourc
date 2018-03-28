f7d2d57a0e570ac389bb68d29eed0c9869c44c3a
wzh <wangzheng422@foxmail.com>
2018-03-28 20:45:00 +0800

    oauth

--- user-service/src/main/java/demo/api/v1/UserControllerV1.java
+++ user-service/src/main/java/demo/api/v1/UserControllerV1.java
@@ -1,18 +1,21 @@
 package demo.api.v1;
 
-import demo.user.User;
+import java.security.Principal;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.security.Principal;
-import java.util.Optional;
+import demo.user.User;
 
 @RestController
-@RequestMapping(path = "/v1")
+@RequestMapping(path = "/v1", method = RequestMethod.GET, 
+produces = "application/json; charset=utf-8")
 public class UserControllerV1 {
 
     private UserServiceV1 userService;
@@ -22,7 +25,8 @@ public class UserControllerV1 {
         this.userService = userService;
     }
 
-    @RequestMapping(path = "/me")
+    @RequestMapping(path = "/me", method = RequestMethod.GET, 
+    produces = "application/json; charset=utf-8")
     public ResponseEntity me(Principal principal) {
         User user = null;
         if(principal != null) {
--- user-service/src/main/java/demo/login/UserController.java
+++ user-service/src/main/java/demo/login/UserController.java
@@ -1,13 +1,15 @@
 package demo.login;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import java.security.Principal;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
-    @RequestMapping("/user")
+    @RequestMapping(value="/user", method = RequestMethod.GET, 
+    produces = "application/json; charset=utf-8")
     public Principal user(Principal user) {
         return user;
     }
