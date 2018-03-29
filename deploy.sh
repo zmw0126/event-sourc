7b7ed858ee594bc085222f1ff0e690f44e0d6ec0
wzh <wangzheng422@foxmail.com>
2018-03-29 13:23:23 +0800

    auto

--- deploy.sh
+++ deploy.sh
@@ -21,18 +21,20 @@ docker push 10.11.0.8:5000/kbastani-account-service
 docker push 10.11.0.8:5000/kbastani-order-service                
 docker push 10.11.0.8:5000/kbastani-inventory-service  
 
-curl -X DELETE "http://10.11.0.4:20081/v1//applications/alauda/event" \
+curl -X DELETE "http://10.11.0.4:20081/v1/applications/alauda/event" \
 -H 'Content-Type: application/json' \
 -H 'Authorization: Token 3051d656b998939335d0c25c3fb9c9ae0c6a6610' \
 -d '{ }'   
 
 sleep 30
 
-curl -X POST "http://10.11.0.4:20081/v1//applications/alauda" \
+curl -X POST  \
+-H "Content-Type: multipart/form-data;charset=UTF-8" \
 -H "Cache-Control: no-cache"\
 -H 'Authorization: Token 3051d656b998939335d0c25c3fb9c9ae0c6a6610' \
 -F "services=./docker-compose.alauda.yml" \
 -F "app_name=event"   \
 -F "region=alauda17"   \
--F "space_name=global"   
+-F "space_name=global"   \
+"http://10.11.0.4:20081/v1/applications/alauda"
 
