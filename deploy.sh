e848d39887817a6029d82168930de8c2eb373e30
wzh <wangzheng422@foxmail.com>
2018-03-29 14:25:33 +0800

    auto

--- deploy.sh
+++ deploy.sh
@@ -3,23 +3,23 @@
 set -e
 set -x
 
-mvn clean
+# mvn clean
 
-mvn package -Dmaven.test.skip=true
+# mvn package -Dmaven.test.skip=true
 
-docker push 10.11.0.8:5000/kbastani-hystrix-dashboard       
-docker push 10.11.0.8:5000/kbastani-shopping-cart-service   
-docker push 10.11.0.8:5000/kbastani-edge-service            
-docker push 10.11.0.8:5000/kbastani-catalog-service         
-docker push 10.11.0.8:5000/kbastani-catalog-service             
-docker push 10.11.0.8:5000/kbastani-online-store-web             
-docker push 10.11.0.8:5000/kbastani-discovery-service            
-docker push 10.11.0.8:5000/kbastani-config-service               
-docker push 10.11.0.8:5000/kbastani-payment-service              
-docker push 10.11.0.8:5000/kbastani-user-service                 
-docker push 10.11.0.8:5000/kbastani-account-service              
-docker push 10.11.0.8:5000/kbastani-order-service                
-docker push 10.11.0.8:5000/kbastani-inventory-service  
+# docker push 10.11.0.8:5000/kbastani-hystrix-dashboard       
+# docker push 10.11.0.8:5000/kbastani-shopping-cart-service   
+# docker push 10.11.0.8:5000/kbastani-edge-service            
+# docker push 10.11.0.8:5000/kbastani-catalog-service         
+# docker push 10.11.0.8:5000/kbastani-catalog-service             
+# docker push 10.11.0.8:5000/kbastani-online-store-web             
+# docker push 10.11.0.8:5000/kbastani-discovery-service            
+# docker push 10.11.0.8:5000/kbastani-config-service               
+# docker push 10.11.0.8:5000/kbastani-payment-service              
+# docker push 10.11.0.8:5000/kbastani-user-service                 
+# docker push 10.11.0.8:5000/kbastani-account-service              
+# docker push 10.11.0.8:5000/kbastani-order-service                
+# docker push 10.11.0.8:5000/kbastani-inventory-service  
 
 curl -X DELETE "http://10.11.0.4:20081/v1/applications/alauda/event" \
 -H 'Content-Type: application/json' \
@@ -29,8 +29,8 @@ curl -X DELETE "http://10.11.0.4:20081/v1/applications/alauda/event" \
 sleep 30
 
 curl -X POST  \
--H "Content-Type: multipart/form-data;charset=UTF-8" \
--H "Cache-Control: no-cache"\
+-H "Content-Type: multipart/form-data;charset=UTF-8"  \
+-H "Cache-Control: no-cache"   \
 -H 'Authorization: Token 3051d656b998939335d0c25c3fb9c9ae0c6a6610' \
 -F "services=./docker-compose.alauda.yml" \
 -F "app_name=event"   \
