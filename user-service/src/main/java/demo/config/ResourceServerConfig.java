147cc6c74ae0e145f5fc37c3280b0937af4802e1
wzh <wangzheng422@foxmail.com>
2018-03-30 18:45:26 +0800

    oauth

--- user-service/src/main/java/demo/config/ResourceServerConfig.java
+++ user-service/src/main/java/demo/config/ResourceServerConfig.java
@@ -14,7 +14,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     public void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/resources/**", "/login","/user").permitAll()
+                .antMatchers("/resources/**", "/login","/me").permitAll()
                 .anyRequest()
                 .authenticated();
     }
