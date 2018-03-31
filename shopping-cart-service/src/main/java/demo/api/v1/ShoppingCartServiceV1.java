fd1eee2e8289b22eb82cfa0e9c369f980ec13f2e
wzh <wangzheng422@foxmail.com>
2018-03-31 09:55:04 +0800

    shopping car

--- shopping-cart-service/src/main/java/demo/api/v1/ShoppingCartServiceV1.java
+++ shopping-cart-service/src/main/java/demo/api/v1/ShoppingCartServiceV1.java
@@ -1,21 +1,7 @@
 package demo.api.v1;
 
-import demo.cart.*;
-import demo.catalog.Catalog;
-import demo.inventory.Inventory;
-import demo.order.Order;
-import demo.order.OrderEvent;
-import demo.order.OrderEventType;
-import demo.user.User;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.oauth2.client.OAuth2RestTemplate;
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-import reactor.core.publisher.Flux;
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
 
 import java.util.Arrays;
 import java.util.List;
@@ -23,8 +9,29 @@ import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
-import static java.util.stream.Collectors.counting;
-import static java.util.stream.Collectors.groupingBy;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestTemplate;
+
+import demo.cart.CartEvent;
+import demo.cart.CartEventRepository;
+import demo.cart.CartEventType;
+import demo.cart.CheckoutResult;
+import demo.cart.LineItem;
+import demo.cart.ShoppingCart;
+import demo.catalog.Catalog;
+import demo.inventory.Inventory;
+import demo.order.Order;
+import demo.order.OrderEvent;
+import demo.order.OrderEventType;
+import demo.user.User;
+import reactor.core.publisher.Flux;
 
 /**
  * The {@link ShoppingCartServiceV1} implements business logic for aggregating the state of
@@ -95,6 +102,7 @@ public class ShoppingCartServiceV1 {
      * @return an aggregate object derived from events performed by the user
      * @throws Exception
      */
+    @Transactional
     public ShoppingCart getShoppingCart() throws Exception {
         User user = oAuth2RestTemplate.getForObject("http://user-service/uaa/v1/me", User.class);
         ShoppingCart shoppingCart = null;
