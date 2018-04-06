2aafaba3ee162027a2fc27219c65a60d2d299b0a
wzh <wangzheng422@foxmail.com>
2018-04-06 23:15:56 +0800

    deploy update to 1.11

--- deploy.sh
+++ deploy.sh
@@ -3,43 +3,45 @@
 set -e
 set -x
 
+export REGISTRY=10.11.0.6:5000
+
 mvn clean
 
 mvn package -Dmaven.test.skip=true
 
-docker push 10.11.0.8:5000/kbastani-hystrix-dashboard       
-docker push 10.11.0.8:5000/kbastani-shopping-cart-service   
-docker push 10.11.0.8:5000/kbastani-edge-service            
-docker push 10.11.0.8:5000/kbastani-catalog-service         
-docker push 10.11.0.8:5000/kbastani-catalog-service             
-docker push 10.11.0.8:5000/kbastani-online-store-web             
-docker push 10.11.0.8:5000/kbastani-discovery-service            
-docker push 10.11.0.8:5000/kbastani-config-service               
-docker push 10.11.0.8:5000/kbastani-payment-service              
-docker push 10.11.0.8:5000/kbastani-user-service                 
-docker push 10.11.0.8:5000/kbastani-account-service              
-docker push 10.11.0.8:5000/kbastani-order-service                
-docker push 10.11.0.8:5000/kbastani-inventory-service  
+docker push ${REGISTRY}/kbastani-hystrix-dashboard       
+docker push ${REGISTRY}/kbastani-shopping-cart-service   
+docker push ${REGISTRY}/kbastani-edge-service            
+docker push ${REGISTRY}/kbastani-catalog-service         
+docker push ${REGISTRY}/kbastani-catalog-service             
+docker push ${REGISTRY}/kbastani-online-store-web             
+docker push ${REGISTRY}/kbastani-discovery-service            
+docker push ${REGISTRY}/kbastani-config-service               
+docker push ${REGISTRY}/kbastani-payment-service              
+docker push ${REGISTRY}/kbastani-user-service                 
+docker push ${REGISTRY}/kbastani-account-service              
+docker push ${REGISTRY}/kbastani-order-service                
+docker push ${REGISTRY}/kbastani-inventory-service  
 
-curl -X DELETE "http://10.11.0.4:20081/v1/applications/alauda/event" \
--H 'Content-Type: application/json' \
--H 'Authorization: Token 3051d656b998939335d0c25c3fb9c9ae0c6a6610' \
--d '{ }' 
+# curl -X DELETE "http://10.11.0.4:20081/v1/applications/alauda/event" \
+# -H 'Content-Type: application/json' \
+# -H 'Authorization: Token 3051d656b998939335d0c25c3fb9c9ae0c6a6610' \
+# -d '{ }' 
 
-echo  
+# echo  
 
-sleep 15
+# sleep 15
 
-curl -X POST  \
--H "Content-Type: multipart/form-data;charset=UTF-8"  \
--H "Cache-Control: no-cache"   \
--H 'Authorization: Token 3051d656b998939335d0c25c3fb9c9ae0c6a6610' \
--F "services=@./docker-compose.alauda.yml" \
--F "app_name=event"   \
--F "region=alauda17"   \
--F "space_name=global"   \
-"http://10.11.0.4:20081/v1/applications/alauda"
+# curl -X POST  \
+# -H "Content-Type: multipart/form-data;charset=UTF-8"  \
+# -H "Cache-Control: no-cache"   \
+# -H 'Authorization: Token 3051d656b998939335d0c25c3fb9c9ae0c6a6610' \
+# -F "services=@./docker-compose.alauda.yml" \
+# -F "app_name=event"   \
+# -F "region=alauda17"   \
+# -F "space_name=global"   \
+# "http://10.11.0.4:20081/v1/applications/alauda"
 
-echo
+# echo
 
 
