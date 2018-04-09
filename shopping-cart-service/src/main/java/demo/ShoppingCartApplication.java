c770f6f933a619a3152a899c3f11cfc88c97fcda
wzh <wangzheng422@foxmail.com>
2018-04-09 23:05:49 +0800

    swagger

--- shopping-cart-service/src/main/java/demo/ShoppingCartApplication.java
+++ shopping-cart-service/src/main/java/demo/ShoppingCartApplication.java
@@ -1,6 +1,7 @@
 package demo;
 
-import demo.order.Order;
+import static springfox.documentation.builders.RequestHandlerSelectors.withClassAnnotation;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
@@ -19,6 +20,17 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import demo.order.Order;
+import io.swagger.annotations.Api;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.ApiSelectorBuilder;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.UiConfiguration;
+import springfox.documentation.swagger.web.UiConfigurationBuilder;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.service.Contact;
+
 /**
  * The {@link ShoppingCartApplication} is a cloud-native Spring Boot application that manages
  * a bounded context for @{link Customer}, @{link Account}, @{link CreditCard}, and @{link Address}
@@ -33,7 +45,33 @@ import org.springframework.web.client.RestTemplate;
 @EnableResourceServer
 @EnableOAuth2Client
 @EnableHystrix
+@EnableSwagger2
 public class ShoppingCartApplication {
+
+    @Bean
+    public Docket docket() {
+        ApiSelectorBuilder apiSelectorBuilder = new Docket(DocumentationType.SWAGGER_2).select();
+        apiSelectorBuilder.apis(withClassAnnotation(Api.class));
+        return apiSelectorBuilder
+                .build()
+                .pathMapping("/")
+                .useDefaultResponseMessages(false)
+                .apiInfo(
+                    new ApiInfoBuilder().title("shopping-cart-service Doc")
+                    .description("shopping-cart-service Doc")
+                    .version("1.0")
+                    .termsOfServiceUrl("https://github.com/wangzheng422/spring-cloud-event-sourcing-example")
+                    .contact(new Contact("George", "https://github.com/wangzheng422", "wangzheng422@gmail.com"))
+                    .build())
+                .forCodeGeneration(true);
+    }
+
+    @Bean
+	UiConfiguration uiConfig() {
+		       
+        return UiConfigurationBuilder.builder().validatorUrl("").build();
+	}
+
     public static void main(String[] args) {
         SpringApplication.run(ShoppingCartApplication.class, args);
     }
--- user-service/src/main/java/demo/UserApplication.java
+++ user-service/src/main/java/demo/UserApplication.java
@@ -1,16 +1,29 @@
 package demo;
 
+import static springfox.documentation.builders.RequestHandlerSelectors.withClassAnnotation;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.stereotype.Component;
+
+import io.swagger.annotations.Api;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.ApiSelectorBuilder;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.UiConfiguration;
+import springfox.documentation.swagger.web.UiConfigurationBuilder;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.service.Contact;
 
 /**
  * The {@link UserApplication} is a cloud-native Spring Boot application that manages
@@ -26,7 +39,34 @@ import org.springframework.stereotype.Component;
 @EnableEurekaClient
 @EnableHystrix
 @EnableAuthorizationServer
+@EnableSwagger2
 public class UserApplication {
+
+    @Bean
+    public Docket docket() {
+        ApiSelectorBuilder apiSelectorBuilder = new Docket(DocumentationType.SWAGGER_2).select();
+        apiSelectorBuilder.apis(withClassAnnotation(Api.class));
+        return apiSelectorBuilder
+                .build()
+                .pathMapping("/")
+                .useDefaultResponseMessages(false)
+                .apiInfo(
+                    new ApiInfoBuilder().title("user-service Doc")
+                    .description("user-service Doc")
+                    .version("1.0")
+                    .termsOfServiceUrl("https://github.com/wangzheng422/spring-cloud-event-sourcing-example")
+                    .contact(new Contact("George", "https://github.com/wangzheng422", "wangzheng422@gmail.com"))
+                    .build())
+                .forCodeGeneration(true);
+    }
+
+    @Bean
+	UiConfiguration uiConfig() {
+		       
+        return UiConfigurationBuilder.builder().validatorUrl("").build();
+	}
+
+
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
     }
