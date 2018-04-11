941f0128edbea86e47d0a1c5219f6fb55c0f8284
wzh <wangzheng422@foxmail.com>
2018-04-12 00:32:34 +0800

    swagger

--- user-service/src/main/java/demo/config/AuthorizationServerConfig.java
+++ user-service/src/main/java/demo/config/AuthorizationServerConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
@@ -57,6 +58,12 @@ public class AuthorizationServerConfig extends WebSecurityConfigurerAdapter {
         //         .and()
         //         .csrf().disable();
 
+    }
+
+    @Override
+    public void configure(final WebSecurity web) throws Exception {
+        // web.ignoring().antMatchers("/**");
+        web.ignoring().antMatchers("/swagger-ui.html");
     }
 
     @Configuration
